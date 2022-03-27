d7cd52eff74d8454326fc2bb8519a01eb868aeab
Nattapat Pinta <47101447+WHIZENx@users.noreply.github.com>
2022-03-28 04:35:36 +0700

    Create LICENSE

---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAkehytNrQnEdT0fr4SW_iRQ7v78HcTKn0",
+    authDomain: "environmentmenager.firebaseapp.com",
+    databaseURL: "https://environmentmenager.firebaseio.com",
+    projectId: "environmentmenager",
+    storageBucket: "environmentmenager.appspot.com",
+    messagingSenderId: "1079166000567",
+    appId: "1:1079166000567:web:9c16473234f3f1b6cd9c32",
+    measurementId: "G-096R1SBD2T"
+  }
+};
